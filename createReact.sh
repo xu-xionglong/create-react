3de0875a7e90ef117c525f3e5786420063c0a1df
xu-xionglong <xionglong.xu@gmail.com>
2019-01-28 19:05:29 +0800

    install @babel/polyfill

--- createReact.sh
+++ createReact.sh
@@ -5,7 +5,7 @@ cd $projectName
 
 npm init -y
 npm install --save-dev webpack webpack-cli webpack-dev-server html-webpack-plugin clean-webpack-plugin
-npm install --save react react-dom
+npm install --save react react-dom @babel/polyfill
 npm install --save-dev babel-cli babel-loader @babel/core @babel/preset-env @babel/preset-react
 
 
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ["@babel/polyfill", './src/index.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build')
